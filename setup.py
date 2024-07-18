bf3aa65bd8693fcfd9dbd32974d53cc3cbc9623c
doctorpangloss <@hiddenswitch.com>
2024-07-18 15:50:39 -0700

    Fix keypoint import

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ import os.path
 packages = find_packages(where='src')
 setup(
     name="comfyui_controlnet_aux",
-    version="0.0.1",
     packages=packages,
     package_dir={"": "src"},
     install_requires=open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines(),
     author='',
     author_email='',
-    description='',
     entry_points={
         'comfyui.custom_nodes': [
             'comfyui_controlnet_aux = comfyui_controlnet_aux.nodes',
